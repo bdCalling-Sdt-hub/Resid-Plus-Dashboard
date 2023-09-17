8b00bef08434f86e40562d293ae97f3ebbf81bb9
Sahinur Islam <infosahinur@gmail.com>
2023-09-17 15:22:53 +0600

    booking page fix

--- src/Pages/Dashboard/Booking/Booking.jsx
+++ src/Pages/Dashboard/Booking/Booking.jsx
@@ -1,9 +1,9 @@
-import { Col, Row } from "antd";
+import { Col, Row,Pagination } from "antd";
 import React from "react";
 import styles from "./Booking.module.css";
 import { LiaHandHoldingUsdSolid } from "react-icons/lia";
 import BookingHistoryTable from "./BookingHistoryTable";
-import { Form, Select } from 'antd';
+import { Form, Select } from "antd";
 
 const data = [
   {
@@ -56,28 +56,100 @@ const data = [
     image: "https://i.ibb.co/txjPMvX/Max-R-Headshot-1.jpg",
     BookingCompleted: 1,
   },
-]
+];
 
 const Booking = () => (
   <div style={{ padding: "0px 10px" }}>
     <h2 style={{ fontSize: "25px", fontWeight: "normal" }}>Bookings</h2>
     <Row gutter={16} style={{ marginTop: "20px" }}>
-      <Col className="gutter-row" style={{ marginBottom: "10px" }} xs={{ span: 24 }} sm={{ span: 24 }} md={{ span: 12 }} lg={{ span: 8 }}>
-        <div className='completed-card'>
-          <h1 style={{ fontSize: "1.5rem", fontWeight: "100", marginTop: "30px", marginBottom: "30px" }}><LiaHandHoldingUsdSolid style={{ fontSize: "24px" }} /> Completed</h1>
-          <h3 style={{ fontSize: "1.5rem", letterSpacing: ".2rem", marginBottom: "15px" }}>250</h3>
+      <Col
+        className="gutter-row"
+        style={{ marginBottom: "10px" }}
+        xs={{ span: 24 }}
+        sm={{ span: 24 }}
+        md={{ span: 12 }}
+        lg={{ span: 8 }}
+      >
+        <div className="completed-card">
+          <h1
+            style={{
+              fontSize: "1.5rem",
+              fontWeight: "100",
+              marginTop: "30px",
+              marginBottom: "30px",
+            }}
+          >
+            <LiaHandHoldingUsdSolid style={{ fontSize: "24px" }} /> Completed
+          </h1>
+          <h3
+            style={{
+              fontSize: "1.5rem",
+              letterSpacing: ".2rem",
+              marginBottom: "15px",
+            }}
+          >
+            250
+          </h3>
         </div>
       </Col>
-      <Col className="gutter-row" style={{ marginBottom: "10px" }} xs={{ span: 24 }} sm={{ span: 24 }} md={{ span: 12 }} lg={{ span: 8 }}>
-        <div className='reserved-card'>
-          <h1 style={{ fontSize: "1.5rem", fontWeight: "100", marginTop: "30px", marginBottom: "30px" }}><LiaHandHoldingUsdSolid style={{ fontSize: "24px" }} /> Reserved</h1>
-          <h3 style={{ fontSize: "1.5rem", letterSpacing: "1px", marginBottom: "15px" }}>250</h3>
+      <Col
+        className="gutter-row"
+        style={{ marginBottom: "10px" }}
+        xs={{ span: 24 }}
+        sm={{ span: 24 }}
+        md={{ span: 12 }}
+        lg={{ span: 8 }}
+      >
+        <div className="reserved-card">
+          <h1
+            style={{
+              fontSize: "1.5rem",
+              fontWeight: "100",
+              marginTop: "30px",
+              marginBottom: "30px",
+            }}
+          >
+            <LiaHandHoldingUsdSolid style={{ fontSize: "24px" }} /> Reserved
+          </h1>
+          <h3
+            style={{
+              fontSize: "1.5rem",
+              letterSpacing: "1px",
+              marginBottom: "15px",
+            }}
+          >
+            250
+          </h3>
         </div>
       </Col>
-      <Col className="gutter-row" style={{ marginBottom: "10px" }} xs={{ span: 24 }} sm={{ span: 24 }} md={{ span: 12 }} lg={{ span: 8 }}>
-        <div className='canceled-card'>
-          <h1 style={{ fontSize: "1.5rem", fontWeight: "100", marginTop: "30px", marginBottom: "30px" }}><LiaHandHoldingUsdSolid style={{ fontSize: "24px" }} /> Canceled</h1>
-          <h3 style={{ fontSize: "1.5rem", letterSpacing: "1px", marginBottom: "15px" }}>$ 250</h3>
+      <Col
+        className="gutter-row"
+        style={{ marginBottom: "10px" }}
+        xs={{ span: 24 }}
+        sm={{ span: 24 }}
+        md={{ span: 12 }}
+        lg={{ span: 8 }}
+      >
+        <div className="canceled-card">
+          <h1
+            style={{
+              fontSize: "1.5rem",
+              fontWeight: "100",
+              marginTop: "30px",
+              marginBottom: "30px",
+            }}
+          >
+            <LiaHandHoldingUsdSolid style={{ fontSize: "24px" }} /> Canceled
+          </h1>
+          <h3
+            style={{
+              fontSize: "1.5rem",
+              letterSpacing: "1px",
+              marginBottom: "15px",
+            }}
+          >
+            $ 250
+          </h3>
         </div>
       </Col>
     </Row>
@@ -86,23 +158,37 @@ const Booking = () => (
       Booking History
     </h2>
 
-
-    <div className={styles.card}>
-      <div className={styles.filterContainer}>
-        <h3 className={styles.cardTitle}>Booking List/ August, 2023</h3>
-        <Form.Item label="" className={styles.filterLabel}>
-          <Select
-            placeholder="Monthly Booking"
-          >
-            <Option value="male">Augest/2023</Option>
-            <Option value="female">September/2023</Option>
-            <Option value="other">October/2023</Option>
-          </Select>
-        </Form.Item>
+    <div>
+      <div className={styles.UserCardContainer}>
+        <div className={styles.filterContainer}>
+          <h3 className={styles.cardTitle}>Booking List/ August, 2023</h3>
+          <Form.Item label="" className={styles.filterLabel}>
+            <Select placeholder="Monthly Booking">
+              <Option value="male">Augest/2023</Option>
+              <Option value="female">September/2023</Option>
+              <Option value="other">October/2023</Option>
+            </Select>
+          </Form.Item>
+        </div>
+        {data.map((item) => (
+          <BookingHistoryTable key={item.id} data={item} />
+        ))}
+        <Row className={styles.Pagination}>
+          <Col>
+            <h1 style={{ fontSize: "20px", color: "#333333" }}>
+              Showing 1-10 OF 250
+            </h1>
+          </Col>
+          <Col>
+            <Pagination
+              defaultCurrent={1}
+              total={5000}
+              showQuickJumper={false}
+              showSizeChanger={false}
+            />
+          </Col>
+        </Row>
       </div>
-      {data.map((item) => (
-        <BookingHistoryTable key={item.id} data={item} />
-      ))}
     </div>
 
     {/* <BookingHistoryTable /> */}
