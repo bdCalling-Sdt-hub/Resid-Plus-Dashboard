376963c295bc8a028ec69251bc297aba019f7b6f
Sahinur Islam <infosahinur@gmail.com>
2023-09-17 18:02:18 +0600

    [ADD] host kyc form

--- src/Pages/Dashboard/HostKyc/HostKyc.jsx
+++ src/Pages/Dashboard/HostKyc/HostKyc.jsx
@@ -1,32 +1,94 @@
-import { Button, Col, Input, Row } from 'antd'
-import React from 'react'
-import { UserOutlined,SearchOutlined } from '@ant-design/icons';
-import HostKycTable from './HostKycTable';
+import { Button, Col, Radio, Input, Row, DatePicker } from "antd";
+import ImgCrop from "antd-img-crop";
+import dayjs from "dayjs";
+import React, { useState } from "react";
+import { LiaEditSolid } from "react-icons/lia";
+import { PlusOutlined } from "@ant-design/icons";
+const dateFormat = "YYYY-MM-DD";
+
+const { RangePicker } = DatePicker;
+
+const { TextArea } = Input;
+import styles from "./HostKyc.module.css";
+
 function HostKyc() {
+  const [value, setValue] = useState("");
+
   return (
     <div>
-        <Row style={{marginBottom:"30px"}}>
-           <h2 style={{ fontSize: "30px", margin: "30px 0px" }}>
-            KYC Settings
-           </h2>  
-             <Col lg={{span:24}}>
-                <div className='' style={{display:"flex",gap:"15px"}}>
-                    <Input size="large" placeholder="Search by name/email/phone" prefix={<SearchOutlined style={{color:"#cccccc"}}/>} />
-                    <Button style={{height:"50px",width:"300px",backgroundColor:"#000b90",color:"#fff",fontSize:"20px"}}>Search</Button>
-                </div>
-             </Col>
-         </Row>
+      <h2 className={styles.hostTitle}>Host KYC Form</h2>
 
-         <Row>
-           <h2 style={{ fontSize: "30px", margin: "30px 0px" }}>
-            Host KYC List
-           </h2>  
-            <Col lg={{span:24}}>
-                <HostKycTable/>
-            </Col>
-         </Row>
+      <div className={styles.formContainer}>
+        <Row style={{ marginBottom: "15px" }}>
+          <Col span={24}>
+            <label htmlFor="">Name</label>
+            <Input
+              style={{ height: "45px", marginTop: "5px" }}
+              placeholder="Enter your name"
+            />
+          </Col>
+        </Row>
+        <Row style={{ marginBottom: "15px" }}>
+          <Col span={24}>
+            <label htmlFor="">Email</label>
+            <Input
+              style={{ height: "45px", marginTop: "5px" }}
+              placeholder="Enter your email"
+            />
+          </Col>
+        </Row>
+        <div className={styles.margeBtn}>
+          <Row style={{ marginBottom: "15px" }}>
+            <label htmlFor="">Date of Birth</label>
+
+            <DatePicker
+              style={{ height: "45px", marginTop: "5px", width: "100%" }}
+              defaultValue={dayjs("2015/01/01", dateFormat)}
+              format={dateFormat}
+            />
+          </Row>
+          <Row style={{ marginBottom: "15px" }}>
+            <label htmlFor="">Phone Number</label>
+            <Input
+              type="number"
+              style={{ height: "45px", marginTop: "5px", width: "100%" }}
+              placeholder="Enter your number"
+            />
+          </Row>
+        </div>
+        <Row style={{ marginBottom: "15px" }}>
+          <Col span={24}>
+            <label htmlFor="">Address</label>
+            <TextArea
+              style={{ marginTop: "5px" }}
+              value={value}
+              onChange={(e) => setValue(e.target.value)}
+              placeholder="Enter your address"
+              autoSize={{ minRows: 3, maxRows: 5 }}
+            />
+          </Col>
+        </Row>
+        <Row style={{ marginBottom: "15px" }}>
+          <Col span={24}>
+            <label htmlFor="">Password</label>
+            <Input.Password
+              style={{ height: "45px", marginTop: "5px" }}
+              placeholder="Password"
+            />
+          </Col>
+        </Row>
+        <Row style={{ marginBottom: "15px" }}>
+          <Col span={24}>
+            <label htmlFor="">Confirm Password</label>
+            <Input.Password
+              style={{ height: "45px", marginTop: "5px" }}
+              placeholder="Confirm Password"
+            />
+          </Col>
+        </Row>
+      </div>
     </div>
-  )
+  );
 }
 
-export default HostKyc
+export default HostKyc;
